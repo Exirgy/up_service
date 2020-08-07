b76591dd22095e3486ba30e35df22ac370bfd0bc
neezystudio <victorneez6@gmail.com>
2020-08-07 23:06:30 +0300

    category grid view

--- lib/pages/layers/UI/sign_in.dart
+++ lib/pages/layers/UI/sign_in.dart
@@ -7,6 +7,7 @@ import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:provider/provider.dart';
 import 'package:up_service/pages/layers/UI/menu_dashboard_layout.dart';
+import 'package:up_service/pages/layers/UI/pages/categories.dart';
 import 'package:up_service/pages/layers/UI/pages/messages.dart';
 
 import '../../../state/app.state.dart';
@@ -69,13 +70,14 @@ class _SignInState extends State<SignIn> {
                 children: [
                   GoogleSignInButton(
                     onPressed: () async {
-                      var response = await userState.handleSignIn();
-                      response
-                          ? Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => MenuDashboardLayout()))
-                          : null;
+                      userState.handleSignIn();
+                      await Future.delayed(Duration(seconds: 10), () {
+                        // 5s over, navigate to a new page
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CategoriesPage()));
+                      });
                     },
                   ),
                   Text(
